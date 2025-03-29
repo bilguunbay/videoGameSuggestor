fb6d44282e8f4bbaa6ec22c5c6da95bda611c871
Bilguun <bilguunb@berkeley.edu>
2025-03-28 18:07:15 -0700

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+// React uses PascalCasing, where each function has each first letter of every word is capitalized
+function Message() {
+    return <h1>Hello World</h1>; // This is JSX: JavaScript XML and not HTML code within JS
+}
+
+export default Message;
